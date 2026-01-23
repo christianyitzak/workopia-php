c26b32c0ca50da7b7c5ac79870dd9b2c37b9d2b9
christianyitzak <christian.yitzak@gmail.com>
2026-01-23 09:53:15 +0700

    create database class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require '../helpers.php';
+require basePath('Database.php');
 require basePath('Router.php');
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
 
 $router = new Router();
 $routes = require basePath('routes.php');
