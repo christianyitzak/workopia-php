a11ef2b9bdafc22138c9b02928d3ca2d4b6bf633
christianyitzak <christian.yitzak@gmail.com>
2026-01-23 10:09:26 +0700

    make query method and try fetch listings

--- public/index.php
+++ public/index.php
@@ -3,12 +3,15 @@ require '../helpers.php';
 require basePath('Database.php');
 require basePath('Router.php');
 
-$config = require basePath('config/db.php');
-$db = new Database($config);
-
+// Instantiate the router
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get the current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
+
+// Route the request
 $router->route($uri, $method);
