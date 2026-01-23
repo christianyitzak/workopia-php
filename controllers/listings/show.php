ae9c58d8ca99ab7a5f4bf1f348414784c0a67bf8
christianyitzak <christian.yitzak@gmail.com>
2026-01-23 11:08:26 +0700

    show data for single listing

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -9,5 +9,7 @@ $params = [
 ];
 
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
-inspect($listing);
-loadView('/listings/show');
+
+loadView('/listings/show', [
+  'listing' => $listing,
+]);
